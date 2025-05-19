b930ddf42178f183695318fb8842d93e2fd544dc
Birat <thapabirat95@gmail.com>
2025-05-19 11:22:41 +0545

    sloved cors issue

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
-
-const bookRoute = require("./routes/book.route");
-
 const app = express();
+
+const cors = require("cors");
+const bookRoute = require("./routes/book.route");
 
 require("./database/connection");
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/books", bookRoute);
 
